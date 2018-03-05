2c058a212efef943d69893605f0b4f9a2d3bec5a
MatthiasKiller <matthias_k@gmx.net>
2018-03-05 18:11:01 +0100

    add new modul out and add import of modul in main

--- main.js
+++ main.js
@@ -1,29 +1,6 @@
 const readline = require('readline');
-
-const figlet = require('figlet');
-const chalk = require('chalk');
-
 const model = require('./model');
-
-const colorize = (msg, color) => {
-    if(typeof color !== "undefined") {
-      msg = chalk[color].bold(msg);
-    }
-    return msg;
-};
-
-const log = (msg, color) => {
-  console.log(colorize(msg,color));
-};
-
-
-const biglog = (msg, color) => {
-  log(figlet.textSync(msg,{horizontalLayout: 'full'}), color);
-};
-
-const errorlog = (emsg) => {
-  console.log(`${colorize("Error","red")}: ${colorize(colorize(emsg, "red"), "bgYellowBright" )}`);
-};
+const {log, biglog, errorlog, colorize} = require('./out');
 
 biglog('CORE Quiz', 'green');
 
