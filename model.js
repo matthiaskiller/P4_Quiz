79682f095b0e2a3d569787b7195ca2b7f9be8ee3
MatthiasKiller <matthias_k@gmx.net>
2018-03-22 19:47:26 +0100

    model.js changed to sql file and Sequelize added

--- model.js
+++ model.js
@@ -1,3 +1,42 @@
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize("sqlite:quizzes.sqlite", {logging: false});
+
+
+sequelize.define('quiz', {
+    question: {
+      type: Sequelize.STRING,
+      unique: {msg: "Ya existe este pregunta"},
+      validate: {notEmpty: {msg: "La pregunta no puede estar vacía"}}
+    },
+    answer: {
+      type: Sequelize.STRING,
+      validate: {notEmpty: {msg: "La pregunta no puede estar vacía"}}
+    }
+});
+
+sequelize.sync()
+.then(() => sequelize.models.quiz.count())
+.then(count => {
+  if(!count) {
+    return sequelize.models.quiz.bulkCreate([
+      {question: "Capital de Italia", answer: "Roma"},
+      {question: "Capital de Francia", answer: "Paris"},
+      {question: "Capital de España", answer: "Madrid"},
+      {question: "Capital de Portugal", answer: "Lisboa"}
+    ]);
+  }
+})
+.catch(error => {
+    console.log(error);
+});
+
+module.exports = sequelize;
+
+
+
+
+/*
+
 const fs = require("fs");
 const DB_FILENAME = "quizzes.json";
 
@@ -103,3 +142,6 @@ exports.deleteByIndex = id => {
 };
 
 load();
+
+
+*/
